3a394d312facc535f1eb1efe31e21b600b1ea937
ChangMin <lcmdev97@gmail.com>
2023-10-14 15:03:57 +0900

    로그인 회원가입 api 완성

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,28 +1,29 @@
-import {sendJson} from "@/pages/utils/sendJson";
 import DB from "@/pages/utils/db";
+import CryptoJS from "crypto-js";
 
 export default async function handler(req, res) {
   const method = req.method
 
-  if(method === "POST") {
+  if (method === "POST") {
     const {id, password} = req.body
+    if (!id || !password) return res.json({code: 400, message: "bad request"})
+
+    const hashedPassword = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, process.env.HASH_KEY).update(password).finalize().toString(CryptoJS.enc.Hex);
 
     const db = DB()
 
-    // await db('users').insert({
-    //   id: 'test1',
-    //   name: 'test1',
-    //   password: 'asd123'
-    // })
-
     const user = await db('users')
+        .where("id", id)
+        .where("password", hashedPassword)
+        .first()
+
+    if (!user) return res.json({code: 404, message: "Not Found User"})
 
     return res.json({
       code: 200,
       message: 'ok'
     })
-    return sendJson(200, 'ok', user)
-    // return sendJson(200, 'ok', user)
   }
+  
   return res.send("Not Allowed Method")
 }
--- pages/api/user.js
+++ pages/api/user.js
@@ -1,23 +1,41 @@
 import DB from "@/pages/utils/db";
+import CryptoJS from 'crypto-js';
+
 
 export default async function handler(req, res) {
   const method = req.method
 
-  if(method === "POST") {
-    const {id, password, confirmPassword } = req.body
+  if (method === "POST") {
+    const {id, password, confirmPassword} = req.body
 
-    if(!id || !password || !confirmPassword || password !== confirmPassword) {
-      return res.json({code: 400, message: "bad request"})
-    }
+    if (!id || !password || !confirmPassword || password !== confirmPassword) return res.json({
+      code: 400,
+      message: "bad request"
+    })
+
+    const hashedPassword = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, process.env.HASH_KEY).update(password).finalize().toString(CryptoJS.enc.Hex);
 
     const db = DB()
 
     const user = await db('users')
+        .where("id", id)
+        .where("password", hashedPassword)
+        .first()
+
+    if (user) return res.json({code: 409, message: "id already in use"})
+
+    let inData = {
+      id,
+      password: hashedPassword,
+    }
+
+    await db('users').insert(inData)
 
     return res.json({
       code: 202,
       message: 'ok'
     })
   }
+
   return res.send("Not Allowed Method")
 }
