ce9a75a86151aa930d4062bbed7173cb28539475
ChangMin <lcmdev97@gmail.com>
2023-10-26 20:58:43 +0900

    로그인 form형식 -> useState + next-auth 이용하는 방식으로 바꿈

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,37 +6,62 @@ import { signIn } from "next-auth/react";
 import styles from "./page.module.css";
 
 export default function Login() {
-  // const [id, setId] = useState("");
-  // const [password, setPassword] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
   const router = useRouter();
-  const onSubmitHandler = async (event) => {
-    event.preventDefault();
-    const id = event.target.id.value;
-    const password = event.target.password.value;
+  // const onSubmitHandler = async (event) => {
+  //   event.preventDefault();
+  //   const id = event.target.id.value;
+  //   const password = event.target.password.value;
+  //
+  //   const result = await signIn("credentials", {
+  //     id,
+  //     password,
+  //     redirect: true,
+  //     callbackUrl: "/",
+  //   });
+  //   console.log("result info:", result);
+  // };
 
-    const result = await signIn("credentials", {
+  const onChangeIdHandler = (event) => {
+    setId(event.target.value);
+  };
+  const onChangePasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onClickLoginBtnHandler = async (event) => {
+    if (!id) return alert("no - id");
+    if (!password) return alert("no - password");
+
+    await signIn("credentials", {
       id,
       password,
       redirect: true,
       callbackUrl: "/",
-    });
-    console.log("result info:", result);
+    })
+      .then((res) => {
+        // console.log("login res info:", res);
+        if (res.data.code >= 400) {
+          return alert(res.data.message);
+        }
+        router.push("/");
+      })
+      .catch((err) => {
+        console.log("err info:", err);
+      });
 
-    // const options = {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: { id, password },
-    // };
-    //
-    // fetch(`http://localhost:3000/login`, options)
-    //   .then((res) => res.json())
-    //   .then((result) => {
-    //     // console.log(result) // {title: 'sss', body: 'ss', id: 3}
-    //     const lastId = result.id;
-    //     router.push(`/read/${lastId}`); // 생성후 생성된 페이지로 이동하기
+    // axios
+    //   .post("http://localhost:3000/api/login", body)
+    //   .then(() => {
+    //     if (res.data.code >= 400) {
+    //       return alert(res.data.message);
+    //     }
+    //     router.push("/");
+    //   })
+    //   .catch((err) => {
+    //     console.log("err info:", err);
     //   });
   };
 
@@ -58,14 +83,22 @@ export default function Login() {
         Your browser does not support the video tag. Please try again in a
         different browser.
       </video>
-      <form onSubmit={onSubmitHandler} className={styles.login_box}>
+      <div className={styles.login_box}>
         <p>계정 로그인</p>
         <p>계정 이름</p>
-        <input type="text" name="id" />
+        <input type="text" value={id} onChange={onChangeIdHandler} />
         <p>비밀번호</p>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={onChangePasswordHandler}
+        />
         <p>
-          <input type="submit" value="로그인" />
+          <input
+            type="button"
+            value="로그인"
+            onClick={onClickLoginBtnHandler}
+          />
         </p>
         <p>
           아직 계정이 없으신가요? <span>지금 가입하세요!</span>
@@ -79,7 +112,7 @@ export default function Login() {
         <p>
           <span>회원가입후 자동 내전 팀짜기 서비스를 이용할 수 있습니다.</span>
         </p>
-      </form>
+      </div>
     </div>
   );
 }
