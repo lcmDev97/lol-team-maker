2022b81ac6477a2d9ea31145a844e442d6ced393
ChangMin <lcmdev97@gmail.com>
2023-11-07 00:32:03 +0900

    인원 추가 버튼에 api 연결중

--- app/component/modal/Modal.jsx
+++ app/component/modal/Modal.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
 import styles from "./Modal.module.css";
 
 function Modal({ closeModal }) {
   let imageTrue = true;
+
+  const [nickname, setNickname] = useState("");
+
+  const onClickAddBtn = async () => {
+    const apiRequest = await axios.post("http://localhost:3000/api/summoner");
+    const result = apiRequest.data;
+
+    if (result.code === 200) {
+      console.log("200 message:", result.message);
+    } else if (result.code === 404) {
+      console.log("404 error:", result.message);
+    } else {
+      console.log("else error:", result.message);
+    }
+  };
 
   return (
     <div className={styles.modal}>
@@ -18,6 +35,7 @@ function Modal({ closeModal }) {
             backgroundImage: "url(/images/icon_reading_glasses.png)",
           }}
           type="text"
+          value={nickname}
           onChange={(event) => {
             if (imageTrue && event.target.value.length > 0) {
               event.target.style.backgroundImage = "none";
@@ -28,12 +46,14 @@ function Modal({ closeModal }) {
               // js로 접근시 /public 생략한 위 경로로 접근해야함
               imageTrue = true;
             }
+            setNickname(event.target.value);
           }}
         />
         <input
           type="button"
           value="추가"
           className={styles.modal_content_add_btn}
+          onClick={onClickAddBtn}
         />
       </div>
       <div className={styles.modal_result_text_div}>
