b8d378523ab4a93a209d3b0c5e7f5bf7150b3c3c
ChangMin <lcmdev97@gmail.com>
2023-12-07 19:32:31 +0900

    DFS 이용한 밸런스 모드 추가

--- pages/api/makeResult.js
+++ pages/api/makeResult.js
@@ -1,32 +1,91 @@
-// function BalanceMode(n, m) {
-//   const team1 = [{ id: 10 }, { id: 11 }];
-//   const noTeamList = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
-//   const answer = [];
-//   const tmp = Array.from({ length: m }, () => 0);
-//
-//   function DFS(L, s) {
-//     if (L === m) {
-//       const clonedTeam1 = team1.slice();
-//       for (const i of tmp) {
-//         clonedTeam1.push(noTeamList[i]);
-//       }
-//       console.log("Cloned team", clonedTeam1);
-//     } else {
-//       for (let i = s; i < n; i++) {
-//         tmp[L] = i;
-//         DFS(L + 1, i + 1);
-//       }
-//     }
-//   }
-//
-//   DFS(0, 0);
-//   return "hi";
-// }
+/* mode의 return값 형태는 아래와 같아야함
+  {
+    finishedTeam1: team1List
+    finishedTeam2: team2List
+  }
+
+  const result = {};
+  result.finishedTeam1 = team1List;
+  result.finishedTeam2 = team2List;
+  return result;
+*/
+
+function BalanceMode(team1List, team2List, noTeamList) {
+  console.log("BalanceMode 실행됨");
+  const n = noTeamList.length; // n(noTeamList 인원수)
+  const m = 5 - team1List.length; // m(team1부족한 인원 수)
+  let totalMmrSum = 0;
+  let team1MmrSum = 0;
+  let max = 0;
+  for (const x of team1List) totalMmrSum += x.mmr;
+  for (const x of team2List) totalMmrSum += x.mmr;
+  for (const x of noTeamList) totalMmrSum += x.mmr;
+  for (const x of team1List) team1MmrSum += x.mmr;
+
+  // console.log("totalMmrSum:", totalMmrSum);
+  // console.log("team1MmrSum:", team1MmrSum);
+  const result = {};
+  const tmp = Array.from({ length: m }, () => 0);
+
+  // console.log(team1List, `필요한 인원 수-${m} : 대기 인원 수 -${n}`);
+
+  function DFS(L, s, mmrSum) {
+    if (L === m) {
+      const tmpTeam2MmrSum = totalMmrSum - mmrSum;
+      console.log(`team1Mmr:${mmrSum} || team2Mmr:${tmpTeam2MmrSum}`);
+      if (max < Math.abs(totalMmrSum - mmrSum)) {
+        max = Math.abs(totalMmrSum - mmrSum);
+        console.log("더 밸런스있는거 발견- team2MmrSum:", max);
+
+        // const clonedTeam1 = team1List.slice()
+        const clonedTeam1 = team1List.map((v) => {
+          // return v.nickname;
+          return v;
+        });
+
+        for (const i of tmp) {
+          // clonedTeam1.push(noTeamList[i].nickname);
+          clonedTeam1.push(noTeamList[i]);
+        }
+        result.finishedTeam1 = clonedTeam1;
+
+        const remainingNoTeam = noTeamList.filter((v, i) => {
+          return !tmp.includes(i);
+          // if (!tmp.includes(i)) return v;
+        });
+        console.log("team2List:", team2List);
+        console.log("remainingNoTeam:", remainingNoTeam);
+        result.finishedTeam2 = [...team2List, ...remainingNoTeam];
+      }
+
+      // // const clonedTeam1 = team1List.slice()
+      // const clonedTeam1 = team1List.map((v) => {
+      //   return v.nickname;
+      // });
+      //
+      // for (const i of tmp) {
+      //   clonedTeam1.push(noTeamList[i].nickname);
+      // }
+      // console.log(`만들어진 팀: ${mmrSum}-${clonedTeam1}`);
+    } else {
+      for (let i = s; i < n; i++) {
+        tmp[L] = i;
+        DFS(L + 1, i + 1, mmrSum + noTeamList[i].mmr);
+      }
+    }
+  }
+
+  DFS(0, 0, team1MmrSum);
+  // console.log("finishedTeam1 info:", result.finishedTeam1);
+  console.log("finishedTeam2 info:", result.finishedTeam2.length);
+  return result;
+}
+
 // console.log(BalanceMode(5, 3)); // n(noTeamList 인원수), m(team1부족한 인원 수)
 
 const RandomMode = (team1List, team2List, noTeamList) => {
   const needCnt = 5 - team1List.length;
-  console.log("needCnt", needCnt);
+  // console.log("needCnt", needCnt);
 
   for (let i = 0; i < needCnt; i++) {
     const randomCnt = parseInt(Math.random() * noTeamList.length);
@@ -68,6 +127,11 @@ export default async function handler(req, res) {
     let result = {};
     if (selectedMode === "random") {
       result = RandomMode(team1List, team2List, noTeamList);
+    } else if (selectedMode === "balance") {
+      result = BalanceMode(team1List, team2List, noTeamList);
+      // result = RandomMode(team1List, team2List, noTeamList);
+    } else {
+      result = {};
     }
 
     return res.json({
