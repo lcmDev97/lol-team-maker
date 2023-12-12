2b3a27e7e23635d2cc0c76622f06bc6549c41fc0
ChangMin <lcmdev97@gmail.com>
2023-12-12 16:15:48 +0900

    riot api 에러 웹훅 추가

--- pages/api/utils/webhook.js
+++ pages/api/utils/webhook.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export function SendTelegramMessage(errorCode, errorMessage, userId) {
+export function SendTelegramMessage(code, message, userId) {
   const telegramApiKey = process.env.TELEGRAM_API_KEY;
   const telegramChatId = process.env.TELEGRAM_CHAT_ID;
   const lineBreak = encodeURI("\n");
-  const text = `[에러발생]${lineBreak}유저 아이디:${userId}${lineBreak}에러코드: ${errorCode}${lineBreak}에러 내용: ${errorMessage}`;
+  const text = `[에러발생]${lineBreak}유저 아이디:${userId}${lineBreak}에러코드: ${code}${lineBreak}에러 내용: ${message}`;
   axios.get(
     `https://api.telegram.org/bot${telegramApiKey}/sendMessage?chat_id=${telegramChatId}&text=${text}`,
   );
