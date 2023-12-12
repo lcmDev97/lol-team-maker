b3922db5cf3259178a76dfdd4c43809eeef20393
ChangMin <lcmdev97@gmail.com>
2023-12-12 19:45:54 +0900

    웹훅 404에러 예외 처리

--- pages/api/utils/webhook.js
+++ pages/api/utils/webhook.js
@@ -5,7 +5,9 @@ export function SendTelegramMessage(code, message, userId) {
   const telegramChatId = process.env.TELEGRAM_CHAT_ID;
   const lineBreak = encodeURI("\n");
   const text = `[에러발생]${lineBreak}유저 아이디:${userId}${lineBreak}에러코드: ${code}${lineBreak}에러 내용: ${message}`;
-  axios.get(
-    `https://api.telegram.org/bot${telegramApiKey}/sendMessage?chat_id=${telegramChatId}&text=${text}`,
-  );
+  if (code && code !== 404) {
+    axios.get(
+      `https://api.telegram.org/bot${telegramApiKey}/sendMessage?chat_id=${telegramChatId}&text=${text}`,
+    );
+  }
 }
