14b18504e5790965bda68b2afafc83b0bd521a58
ChangMin <lcmdev97@gmail.com>
2023-12-15 17:36:18 +0900

    접속자 수 확인 기능 추가중

--- pages/api/utils/webhook.js
+++ pages/api/utils/webhook.js
@@ -4,7 +4,9 @@ export function SendTelegramMessage(code, message, userId) {
   const telegramApiKey = process.env.TELEGRAM_API_KEY;
   const telegramChatId = process.env.TELEGRAM_CHAT_ID;
   const lineBreak = encodeURI("\n");
-  const text = `[에러발생]${lineBreak}유저 아이디:${userId}${lineBreak}에러코드: ${code}${lineBreak}에러 내용: ${message}`;
+
+  const title = code >= 400 ? "에러 발생" : "정보 알림";
+  const text = `[${title}]${lineBreak}유저 아이디:${userId}${lineBreak}에러코드: ${code}${lineBreak}에러 내용: ${message}`;
   if (code && code !== 404) {
     axios.get(
       `https://api.telegram.org/bot${telegramApiKey}/sendMessage?chat_id=${telegramChatId}&text=${text}`,
