60fb283a5923dcd38d8e975b554e8adde075306e
ChangMin <lcmdev97@gmail.com>
2023-12-17 14:54:27 +0900

    배포 테스트

--- app/page.jsx
+++ app/page.jsx
@@ -54,6 +54,18 @@ export default function Home() {
       })
       .then((res) => {
         if (res.data.code === 200) {
+          console.log("처음 받아오는 곳에서 문제인가:", res.data.result);
+          console.log("변환후");
+
+          for (const x of res.data.result) {
+            x.renewaled_at = dayjs(x.renewaled_at)
+              .tz("Asia/Seoul")
+              .format("YYYY-MM-DD HH:mm:ss");
+            x.created_at = dayjs(x.created_at)
+              .tz("Asia/Seoul")
+              .format("YYYY-MM-DD HH:mm:ss");
+          }
+          console.log(res.data.code);
           setFriendList(res.data.result);
         }
       });
